688a2f122bf5f85c936ccc8d90d59660147b8a23
Eternal-could <20578466@qq.com>
2022-02-11 18:56:18 +0800

    feature: 增加富文本编辑器模块wangEditor及highlight.js,文章发布页面

--- src/pages/user/main.js
+++ src/pages/user/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import 'highlight.js/styles/atom-one-dark.css';
 import router from "@/pages/user/router";
 
 Vue.use(ElementUI);
--- src/pages/user/views/Article-Edit.vue
+++ src/pages/user/views/Article-Edit.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>文章编辑页</div>
+  <div>
+    <el-page-header
+      @back="goBack"
+    ></el-page-header>
+    <GlobalEditor></GlobalEditor>
+  </div>
 </template>
 
 <script>
+import GlobalEditor from "@/components/Global-Editor";
 export default {
-  name: "Article-Edit"
+  name: "Article-Edit",
+  components: {
+    GlobalEditor
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
+  }
 }
 </script>
 
