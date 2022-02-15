15b7bca6df3a56a4ad7d45eb8f079707f046692d
Eternal-could <20578466@qq.com>
2022-02-15 15:51:56 +0800

    feature : import echarts

--- src/pages/admin/main.js
+++ src/pages/admin/main.js
@@ -3,7 +3,8 @@ import App from "./App";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from "@/pages/admin/router";
-
+import * as echarts from 'echarts';
+Vue.prototype.$echarts = echarts;
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
