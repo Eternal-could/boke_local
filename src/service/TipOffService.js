0300ce5044e6b518d166497abf7a8d5c0a5181c8
Eternal-could <20578466@qq.com>
2022-02-15 14:44:06 +0800

    feature: 增加管理员界面举报功能

--- src/service/TipOffService.js
+++ src/service/TipOffService.js
@@ -5,6 +5,20 @@ class TipOffService {
     async tipBlog(data) {
         return axios.post(`${defaultConfig.baseApiUrl}/tipOff/article`,data);
     }
+
+    async getTipOffData(args) {
+        return axios.get(`${defaultConfig.baseApiUrl}/tipOff/article`, {
+            params: args
+        });
+    }
+
+    async ignoreTipOffDataById(args) {
+        return axios.delete(`${defaultConfig.baseApiUrl}/tipOff/tipOffData/${args.tipOffId}`);
+    }
+
+    async deleteTipOffDataById(args) {
+        return axios.delete(`${defaultConfig.baseApiUrl}/tipOff/article/${args.tipOffId}`);
+    }
 }
 
 export default new TipOffService()
